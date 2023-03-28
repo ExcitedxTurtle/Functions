422480466a7537d869e8757130523f5177d0cdae
ExcitedxTurtle <124044194+ExcitedxTurtle@users.noreply.github.com>
2023-03-28 10:50:24 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,8 +11,8 @@ function Full_Forwards () {
     basic.pause(100)
 }
 function Soft_Left () {
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 60)
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 20)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 60)
 }
 function Distance_Stop () {
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
@@ -28,8 +28,8 @@ function Hard_Left () {
     basic.pause(100)
 }
 function Soft_Right () {
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 60)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 60)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 20)
 }
 // should be 90 degrees
 function Hard_Right () {
@@ -42,31 +42,22 @@ function Avoid () {
     basic.pause(200)
     Hard_Right()
     Full_Forwards()
-    while (Avoiding == 1) {
-        if (maqueen.Ultrasonic(PingUnit.Centimeters) < 10) {
-            Soft_Right()
-        } else {
-            Soft_Left()
-        }
-        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 || maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
-            Hard_Right()
-            Avoiding = 0
-        }
-    }
 }
 basic.forever(function () {
     if (maqueen.Ultrasonic(PingUnit.Centimeters) < 10) {
         Avoiding = 1
         Avoid()
+        Soft_Left()
+        basic.pause(200)
     }
     LFSR = maqueen.readPatrol(maqueen.Patrol.PatrolRight)
     LFSL = maqueen.readPatrol(maqueen.Patrol.PatrolLeft)
     if (LFSL == 1 && LFSR == 1) {
         Full_Forwards()
     } else if (LFSL == 1) {
-        Soft_Left()
-    } else if (LFSR == 1) {
         Soft_Right()
+    } else if (LFSR == 1) {
+        Soft_Left()
     } else {
         Full_Stop()
     }
